5d3256cd5a08b01e2a1ff30e8f4ca52a75263f14
sundaygabriel <gabrielchinedus@gmail.com>
2022-05-26 11:19:35 +0100

    first commit

---
+++ tesla-clone/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+
+import './App.css';
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Home/>
+
+    </div>
+  );
+}
+
+export default App;
